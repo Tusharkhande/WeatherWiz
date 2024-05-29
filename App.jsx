188cfcc2671142d8490c4897de2b59385478772f
Tusharkhande <khandetushar2001@gmail.com>
2024-05-29 18:44:11 +0530

    added some assets and also set up tailwind css using nativewind

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text className='text-center'>Open up App.tsx to start working on your app!</Text>
+    </View>
+  )
+}
+
+export default App;
